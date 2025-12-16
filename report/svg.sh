fa0264aac1c38232e77d8558e9deb91c82e8c12b
Juan Pablo Medina <jpmedinagl@gmail.com>
2025-12-16 11:15:54 -0500

    finishing report

---
+++ report/svg.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+inkscape svg/metadata_struct1.svg --export-filename=images/metadata_struct1.png --export-dpi=300
+inkscape svg/metadata_struct2.svg --export-filename=images/metadata_struct2.png --export-dpi=300
+inkscape svg/metadata_struct3.svg --export-filename=images/metadata_struct3.png --export-dpi=300
+inkscape svg/data_struct.svg --export-filename=images/data_struct.png --export-dpi=300
+inkscape svg/communication.svg --export-filename=images/communication.png --export-dpi=300
